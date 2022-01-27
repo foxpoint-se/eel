27f929f7c87c1271990e9ed0407a3ef884bb92e8
bryndav <d.bryngelsson@gmail.com>
2022-01-27 14:47:05 +0100

    * Fix unix file ending

--- src/eel/resource/forward_serial.py
+++ src/eel/resource/forward_serial.py
@@ -1,9 +1,27 @@
+#!/usr/bin/python3
+# Script to be put on device with the modem, forwards serial commands, i.e python forward_serial.py AT
+
+import logging
 import serial
+import sys
+
+logger = logging.getLogger(sys.argv[0])
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+port = "/dev/ttyUSB3"
+ser = serial.Serial(port, baudrate=115200, timeout=1)
+
 
 if __name__ == "__main__":
-    
 
-    port = "/dev/ttyUSB0"
-    coms = serial.Serial(port, baudrate=115200)
-
-    coms.write(msg)
+    for arg in sys.argv[1:]:
+        msg = f"{arg}\r".encode("utf-8")
+        logger.info(f"Sending command: {msg}")
+        ser.write(msg)
+        resp = ser.read(128)
+        logger.info(f"Got response {resp.decode()}")
+        ser.close()
