f47c413e3a7e4f4b2fda00b00c60a4cb8e990c39
bulingen <adamlecorney@gmail.com>
2022-03-06 10:46:44 +0100

    refactor: move sim specific code to another class

--- src/eel/eel/gnss/gnss_node.py
+++ src/eel/eel/gnss/gnss_node.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from eel_interfaces.msg import GnssStatus, ImuStatus
-from std_msgs.msg import Float32
-from geopy import distance
+from eel_interfaces.msg import GnssStatus
 from .gnss_sensor import GnssSensor
+from .gnss_sim import GnssSimulator
 
 GNSS_STATUS_TOPIC = "/gnss_status"
-IMU_STATUS_TOPIC = "imu_status"
-MOTOR_TOPIC = "motor"
 SIMULATE_PARAM = "simulate"
 DEBUG_PARAM = "debug"
 
@@ -23,55 +20,19 @@ class GNSS(Node):
         self.debug = self.get_parameter(DEBUG_PARAM).value
 
         if not self.should_simulate:
-            self.poller = self.create_timer(1.0, self.publish)
-            self.gnss = GnssSensor(self.should_simulate)
+            sensor = GnssSensor()
+            self.get_current_position = sensor.get_current_position
         else:
-            self.motor_subscription = self.create_subscription(
-                Float32, MOTOR_TOPIC, self.handle_motor_msg, 10
-            )
-            self.imu_subscription = self.create_subscription(
-                ImuStatus, IMU_STATUS_TOPIC, self.handle_imu_msg, 10
-            )
-            self.poller = self.create_timer(1.0, self.publish_fake_gnss)
-            self.current_position = {"lat": 59.309406850903784, "lon": 17.9742443561554}
-            self.speed = 0
-            self.current_heading = float(0)
+            simulator = GnssSimulator(self)
+            self.get_current_position = simulator.get_current_position
 
+        self.poller = self.create_timer(1.0, self.publish)
         self.get_logger().info(
             "{}GNSS node started.".format("SIMULATE " if self.should_simulate else "")
         )
 
-    def publish_fake_gnss(self):
-        if self.speed > 0:
-            new_position = distance.distance(meters=3.0).destination(
-                (self.current_position["lat"], self.current_position["lon"]),
-                bearing=self.current_heading,
-            )
-            self.current_position["lat"] = new_position.latitude
-            self.current_position["lon"] = new_position.longitude
-            # if self.current_heading > 90 and self.current_heading < 270:
-            #     self.current_position["lat"] -= 0.0001
-            # else:
-            #     self.current_position["lat"] += 0.0001
-
-            # if self.current_heading > 0 and self.current_heading < 180:
-            #     self.current_position["lon"] += 0.0001
-            # else:
-            #     self.current_position["lon"] -= 0.0001
-
-        msg = GnssStatus()
-        msg.lat = self.current_position.get("lat")
-        msg.lon = self.current_position.get("lon")
-        self.publisher.publish(msg)
-
-    def handle_motor_msg(self, msg):
-        self.speed = msg.data
-
-    def handle_imu_msg(self, msg):
-        self.current_heading = msg.euler_heading
-
     def publish(self):
-        lat, lon = self.gnss.get_current_position()
+        lat, lon = self.get_current_position()
 
         if self.debug:
             self.get_logger().info("lat: {}, lon: {}".format(lat, lon))
