c3cad78f304caa5b900756ff2399c26097e6f0c4
bulingen <adamlecorney@gmail.com>
2022-03-06 17:42:16 +0100

    feat: stop simlator from turning when speed is zero

--- src/eel/eel/imu/imu_sim.py
+++ src/eel/eel/imu/imu_sim.py
@@ -2,24 +2,33 @@ from std_msgs.msg import Float32
 from ..utils.translate import translate_from_range_to_range
 
 RUDDER_TOPIC = "rudder"
+MOTOR_TOPIC = "motor"
 
 
 class ImuSimulator:
     def __init__(self, parent_node=None) -> None:
         self.current_rudder_angle = float(0)
         self.current_heading = float(0)
+        self.speed = 0
         self.rudder_subscription = parent_node.create_subscription(
             Float32, RUDDER_TOPIC, self.handle_rudder_msg, 10
+        )
+        self.motor_subscription = parent_node.create_subscription(
+            Float32, MOTOR_TOPIC, self.handle_motor_msg, 10
         )
         self.imu_updater = parent_node.create_timer(1.0, self.update_imu)
 
     def handle_rudder_msg(self, msg):
         self.current_rudder_angle = msg.data * 90
 
+    def handle_motor_msg(self, msg):
+        self.speed = msg.data
+
     def update_imu(self):
-        self.current_heading += translate_from_range_to_range(
-            self.current_rudder_angle, -90.0, 90.0, -10.0, 10.0
-        )
+        if self.speed > 0:
+            self.current_heading += translate_from_range_to_range(
+                self.current_rudder_angle, -90.0, 90.0, -10.0, 10.0
+            )
 
     def get_calibration_status(self):
         sys = 3
