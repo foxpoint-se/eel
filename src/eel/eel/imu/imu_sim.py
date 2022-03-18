1c8ba8a4d9162ed9ae9d7bf92306594af7d8bb0f
bulingen <adamlecorney@gmail.com>
2022-03-18 23:04:53 +0100

    feat: easier configuration of angular velocity in simulation

--- src/eel/eel/imu/imu_sim.py
+++ src/eel/eel/imu/imu_sim.py
@@ -4,6 +4,15 @@ from ..utils.translate import translate_from_range_to_range
 RUDDER_TOPIC = "rudder"
 MOTOR_TOPIC = "motor"
 
+# hertz (updates per second)
+UPDATE_FREQUENCY = 2
+
+# hertz (degrees per second)
+ANGULAR_VELOCITY = 10.0
+
+# divided by UPDATE_FREQUENCY, since faster updates will otherwise make it turn faster
+RUDDER_EXTREME = ANGULAR_VELOCITY / UPDATE_FREQUENCY
+
 
 class ImuSimulator:
     def __init__(self, parent_node=None) -> None:
@@ -11,23 +20,25 @@ class ImuSimulator:
         self.current_heading = float(0)
         self.speed = 0
         self.rudder_subscription = parent_node.create_subscription(
-            Float32, RUDDER_TOPIC, self.handle_rudder_msg, 10
+            Float32, RUDDER_TOPIC, self._handle_rudder_msg, 10
         )
         self.motor_subscription = parent_node.create_subscription(
-            Float32, MOTOR_TOPIC, self.handle_motor_msg, 10
+            Float32, MOTOR_TOPIC, self._handle_motor_msg, 10
         )
-        self.imu_updater = parent_node.create_timer(1.0, self.update_imu)
+        self.imu_updater = parent_node.create_timer(
+            1.0 / UPDATE_FREQUENCY, self._update_imu
+        )
 
-    def handle_rudder_msg(self, msg):
+    def _handle_rudder_msg(self, msg):
         self.current_rudder_angle = msg.data * 90
 
-    def handle_motor_msg(self, msg):
+    def _handle_motor_msg(self, msg):
         self.speed = msg.data
 
-    def update_imu(self):
+    def _update_imu(self):
         if self.speed > 0:
             self.current_heading += translate_from_range_to_range(
-                self.current_rudder_angle, -90.0, 90.0, -10.0, 10.0
+                self.current_rudder_angle, -90.0, 90.0, -RUDDER_EXTREME, RUDDER_EXTREME
             )
 
     def get_calibration_status(self):
