633cca9023f59e590856bb0ad545cde0d53721c0
bulingen <adamlecorney@gmail.com>
2022-03-18 23:10:21 +0100

    chore: clarify which functions are internal

--- src/eel/eel/gnss/gnss_sim.py
+++ src/eel/eel/gnss/gnss_sim.py
@@ -9,28 +9,25 @@ IMU_STATUS_TOPIC = "imu_status"
 class GnssSimulator:
     def __init__(self, parent_node=None) -> None:
         self.motor_subscription = parent_node.create_subscription(
-            Float32, MOTOR_TOPIC, self.handle_motor_msg, 10
+            Float32, MOTOR_TOPIC, self._handle_motor_msg, 10
         )
         self.imu_subscription = parent_node.create_subscription(
-            ImuStatus, IMU_STATUS_TOPIC, self.handle_imu_msg, 10
+            ImuStatus, IMU_STATUS_TOPIC, self._handle_imu_msg, 10
         )
 
-        self.position_updater = parent_node.create_timer(1.0, self.update_position)
+        self.position_updater = parent_node.create_timer(1.0, self._update_position)
 
         self.speed = 0
         self.current_heading = float(0)
         self.current_position = {"lat": 59.309406850903784, "lon": 17.9742443561554}
 
-    def handle_motor_msg(self, msg):
+    def _handle_motor_msg(self, msg):
         self.speed = msg.data
 
-    def handle_imu_msg(self, msg):
+    def _handle_imu_msg(self, msg):
         self.current_heading = msg.euler_heading
 
-    def get_current_position(self):
-        return self.current_position["lat"], self.current_position["lon"]
-
-    def update_position(self):
+    def _update_position(self):
         if self.speed > 0:
             new_position = distance.distance(meters=3.0).destination(
                 (self.current_position["lat"], self.current_position["lon"]),
@@ -38,3 +35,6 @@ class GnssSimulator:
             )
             self.current_position["lat"] = new_position.latitude
             self.current_position["lon"] = new_position.longitude
+
+    def get_current_position(self):
+        return self.current_position["lat"], self.current_position["lon"]
