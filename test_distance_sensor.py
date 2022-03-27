b46f666065121bcd8b35d383ab009a2cf94e9ffc
bulingen <adamlecorney@gmail.com>
2022-03-27 22:06:30 +0200

    fix: add distance testing stuff

--- test_distance_sensor.py
+++ test_distance_sensor.py
@@ -7,6 +7,15 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 
+# vl53.measurement_timing_budget = 200000
+vl53.measurement_timing_budget = 500000
+
 while True:
     print("range", vl53.range, "mm")
     time.sleep(1.0)
+
+# average_count = 10
+# measurements = []
+# while True:
+#     if len(measurements) < average_count:
+#         measurements.append()
