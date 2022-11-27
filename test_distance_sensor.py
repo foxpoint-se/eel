7b766115ce947c699890e26d0cec30b8c3d12e30
bryndav <d.bryngelsson@gmail.com>
2022-11-27 20:04:49 +0100

    *Refactor: Cleaned test scripts

--- test_distance_sensor.py
+++ test_distance_sensor.py
@@ -1,30 +1,32 @@
+import argparse
 import time
 import board
 import busio
 import adafruit_vl53l0x
 import adafruit_bno055
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--front", action="count", help="Reads front distance sensor")
+parser.add_argument("-r", "--rear", action="count", help="Reads rear distance sensor")
+args = parser.parse_args()
+
+
 i2c = busio.I2C(board.SCL, board.SDA)
+sensor_address = None
 
-vl53 = adafruit_vl53l0x.VL53L0X(i2c)
-sensor = adafruit_bno055.BNO055_I2C(i2c)
+if args.rear:
+    sensor_address = 29
 
+if args.front:
+    sensor_address = 22
 
-# vl53.measurement_timing_budget = 200000
+vl53 = adafruit_vl53l0x.VL53L0X(i2c, address=sensor_address)
 vl53.measurement_timing_budget = 500000
 vl53.start_continuous()
-print(f"Is vl53 in cont mode {vl53.is_continuous_mode}")
+print(f"Is vl53 in cont mode {vl53.is_continuous_mode}, on address {sensor_address}")
 
 while True:
-    heading, roll, pitch = sensor.euler
-    print(float(heading or 0), float(roll or 0), float(pitch or 0))
-
     print("range", vl53.range, "mm")
     print("")
-    time.sleep(1.0)
-
-# average_count = 10
-# measurements = []
-# while True:
-#     if len(measurements) < average_count:
-#         measurements.append()
+    time.sleep(0.3)
