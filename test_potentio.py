4254fa41fcc3e1a1993d0b1487eee5692b18a7a9
bulingen <adamlecorney@gmail.com>
2023-01-22 21:51:13 +0100

    fix: run in "single-ended mode"

--- test_potentio.py
+++ test_potentio.py
@@ -8,7 +8,8 @@ from adafruit_ads1x15.analog_in import AnalogIn
 
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1015(i2c)
-chan = AnalogIn(ads, ADS.P0, ADS.P1)
+# chan = AnalogIn(ads, ADS.P0, ADS.P1)
+chan = AnalogIn(ads, ADS.P0)
 
 last_reading = time.time()
 
