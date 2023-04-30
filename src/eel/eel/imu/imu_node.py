c8da0f8d82c47224c4340aa5bc5622c9481bbf58
bulingen <adamlecorney@gmail.com>
2023-04-30 21:51:04 +0000

    feat: publish pitch velocity

--- src/eel/eel/imu/imu_node.py
+++ src/eel/eel/imu/imu_node.py
@@ -2,16 +2,26 @@
 import rclpy
 from rclpy.node import Node
 from eel_interfaces.msg import ImuStatus
+from time import time
 from .imu_sensor import ImuSensor
 from .imu_sim import ImuSimulator
 from ..utils.constants import SIMULATE_PARAM
 from ..utils.topics import IMU_STATUS
 
 
+def get_pitch_velocity(pitch, previous_pitch, now, previous_pitch_at):
+    if previous_pitch is None or previous_pitch_at is None:
+        return 0.0
+    pitch_delta = pitch - previous_pitch
+    time_delta = now - previous_pitch_at
+    velocity = pitch_delta / time_delta
+    return velocity
+
+
 def filter_pitch(previous, new):
     if previous is None:
         return new
-    if abs(new - previous) > 30:
+    if abs(new - previous) > 30.0:
         return previous
     if new > 360.0 or new < -360.0:
         return previous
@@ -25,6 +35,7 @@ class ImuNode(Node):
         self.should_simulate = self.get_parameter(SIMULATE_PARAM).value
         self.status_publisher = self.create_publisher(ImuStatus, IMU_STATUS, 10)
         self.previous_pitch = None
+        self.previous_pitch_at = None
 
         # hertz (publications per second)
         self.update_frequency = 5
@@ -62,13 +73,19 @@ class ImuNode(Node):
             msg.heading = heading
             msg.roll = roll
 
+            now = time()
+
+            pitch_velocity = get_pitch_velocity(pitch, self.previous_pitch, now, self.previous_pitch_at)
+
             pitch_to_send = filter_pitch(self.previous_pitch, pitch)
 
             msg.pitch = pitch_to_send
+            msg.pitch_velocity = pitch_velocity
 
             self.status_publisher.publish(msg)
 
             self.previous_pitch = pitch_to_send
+            self.previous_pitch_at = now
 
         except (OSError, IOError) as err:
             self.get_logger().error(str(err))
