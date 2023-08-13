74372e36e36d777b8f8273cfbbb34b36a1e66f93
bulingen <adamlecorney@gmail.com>
2023-08-13 19:50:39 +0000

    feat: rudder cmd interface to allow for more props

--- src/eel/eel/rudder/rudder_node.py
+++ src/eel/eel/rudder/rudder_node.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
 from std_msgs.msg import Float32
 import sys
+from eel_interfaces.msg import RudderCmd
 from .general_servo import RudderServo
 from .rudder_sim import RudderSimulator
 from ..utils.topics import RUDDER_CMD, RUDDER_STATUS
@@ -24,8 +25,11 @@ class Rudder(Node):
         self.rudder_calibration_term = (
             SIM_CALIBRATION_TERM if self.should_simulate else CALIBRATION_TERM
         )
+        # self.rudder_cmd_subscription = self.create_subscription(
+        #     Float32, RUDDER_CMD, self.handle_rudder_msg, 10
+        # )
         self.rudder_cmd_subscription = self.create_subscription(
-            Float32, RUDDER_CMD, self.handle_rudder_msg, 10
+            RudderCmd, RUDDER_CMD, self.handle_rudder_msg, 10
         )
         self.rudder_status_publisher = self.create_publisher(Float32, RUDDER_STATUS, 10)
 
@@ -45,6 +49,17 @@ class Rudder(Node):
             )
             self.horizontal_detach = horizontal_servo.detach
             self.horizontal_set_value = horizontal_servo.set_value
+            
+            vertical_servo = RudderServo(
+                pin=19,
+                min_pulse_width=0.81 / 1000,
+                max_pulse_width=2.2 / 1000,
+                flip_direction=False,
+                # cap_min = -0.85,
+                # cap_max = 0.85,
+            )
+            self.vertical_detach = vertical_servo.detach
+            self.vertical_set_value = vertical_servo.set_value
 
         self.get_logger().info(
             "{}Rudder node started.".format("SIMULATE " if self.should_simulate else "")
@@ -53,13 +68,18 @@ class Rudder(Node):
     def shutdown(self):
         self.get_logger().info("Rudder node shutting down...")
         self.horizontal_detach()
+        self.vertical_detach()
 
     def handle_rudder_msg(self, msg):
-        new_value = msg.data + self.rudder_calibration_term
-        rudder_value = clamp(new_value, -1, 1)
-        self.horizontal_set_value(rudder_value)
+        new_horizontal = msg.horizontal_value + self.rudder_calibration_term
+        new_vertical = msg.vertical_value
+        # new_value = msg.data + self.rudder_calibration_term
+        new_horizontal = clamp(new_horizontal, -1, 1)
+        new_vertical = clamp(new_vertical, -1, 1)
+        self.horizontal_set_value(new_horizontal)
+        self.vertical_set_value(new_vertical)
         status_msg = Float32()
-        status_msg.data = float(rudder_value)
+        status_msg.data = float(new_horizontal) # TODO: status for vertical also
         self.rudder_status_publisher.publish(status_msg)
 
 
