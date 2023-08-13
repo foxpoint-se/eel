e2051683a7d74ad860759ae05d96cac027108f53
bulingen <adamlecorney@gmail.com>
2023-08-13 21:28:42 +0000

    docs: try ad script with another library (works)

--- test_adconverter_mcp3008.py
+++ test_adconverter_mcp3008.py
@@ -2,30 +2,35 @@ import busio
 import digitalio
 import board
 import time
-import adafruit_mcp3xxx.mcp3008 as MCP
+# import adafruit_mcp3xxx.mcp3208 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+from gpiozero import MCP3208
+
+# channel 1 is rear (?) and 0 is front (?)
+mcp = MCP3208(channel=1, differential=False, max_voltage=3.3)
 
 # create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+# spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
 # create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
+# cs = digitalio.DigitalInOut(board.D5)
 
 # create the mcp object
-mcp = MCP.MCP3008(spi, cs)
+# mcp = MCP.MCP3208(spi, cs)
 
 # create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P6)
+# chan = AnalogIn(mcp, MCP.P6)
 
-pins = [AnalogIn(mcp, MCP.P0), AnalogIn(mcp, MCP.P1), AnalogIn(mcp, MCP.P2), AnalogIn(mcp, MCP.P3), AnalogIn(mcp, MCP.P4), AnalogIn(mcp, MCP.P5), AnalogIn(mcp, MCP.P6), AnalogIn(mcp, MCP.P7)]
+# pins = [AnalogIn(mcp, MCP.P0), AnalogIn(mcp, MCP.P1), AnalogIn(mcp, MCP.P2), AnalogIn(mcp, MCP.P3), AnalogIn(mcp, MCP.P4), AnalogIn(mcp, MCP.P5), AnalogIn(mcp, MCP.P6), AnalogIn(mcp, MCP.P7)]
 
-print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
-print('-' * 57)
+# print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+# print('-' * 57)
 
 
 while True:
-    values = [0]*8
-    for i, channel in enumerate(pins):
-        values[i] = channel.value
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+    # values = [0]*8
+    # for i, channel in enumerate(pins):
+    #     values[i] = channel.value
+    # print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+    print(mcp.value)
     time.sleep(1)
