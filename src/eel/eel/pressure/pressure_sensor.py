a5bd4b2a024a612fb4cddca9eaba89f35c9b9c27
bryndav <d.bryngelsson@gmail.com>
2024-01-10 22:29:22 +0100

    Refactor: Modify pressure sensor and node to read values from new I2C serial proxy

--- src/eel/eel/pressure/pressure_sensor.py
+++ src/eel/eel/pressure/pressure_sensor.py
@@ -1,31 +1,25 @@
-import time
-import ms5837
+import serial
+import struct
 from rclpy.node import Node
+
+
+FLOAT_SIZE_IN_BYTES = 4
 
 
 class PressureSensor:
     def __init__(self, parent_node: Node) -> None:
-        self.sensor = ms5837.MS5837_02BA()
-        # TODO: try catch on startup, and try again in some way
-        if not self.sensor.init():
-            raise RuntimeError("Pressure sensor could not be initialized")
+        self.serial_connection = serial.Serial("/dev/ttyUSB1")
+        self.serial_connection.flushInput()
+        self.serial_connection.flushOutput()
 
-        if not self.sensor.read():
-            raise RuntimeError("Pressure sensor cannot read")
-
-        offset_readings = 0
-        
-        # nof_readings = 5
-        # for _ in range(nof_readings):
-        #     offset_readings += self.sensor.depth()
-
-        self.atmosphere_offset = self.sensor.depth()
+        self.atmosphere_offset = self.get_current_depth()
 
         self.logger = parent_node.get_logger()
-        self.sensor.setFluidDensity(ms5837.DENSITY_FRESHWATER)
-        self.logger.info(f"Sensor initialized, fluid density set to {ms5837.DENSITY_FRESHWATER} km/m3, atmosphere offset is {self.atmosphere_offset} m")
+
+        self.logger.info(f"Sensor initialized, fluid density set to 997 km/m3, atmosphere "
+                         f"offset is {self.atmosphere_offset} m")
 
     def get_current_depth(self):
-        self.sensor.read()
-        
-        return self.sensor.depth() - self.atmosphere_offset 
+        depth_as_bytes = self.serial_connection.read(FLOAT_SIZE_IN_BYTES)
+
+        return struct.unpack("f", depth_as_bytes)[0]
