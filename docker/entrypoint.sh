380b89dfec926a0614910d6fe2514bcfb4945969
bulingen <adamlecorney@gmail.com>
2024-01-22 00:06:12 +0100

    chore: remove commented code

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,8 +8,6 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 if [ -f /eel/install/setup.bash ]
 then
   source /eel/install/setup.bash
-#   export TURTLEBOT3_MODEL=waffle_pi
-#   export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix turtlebot3_gazebo)/share/turtlebot3_gazebo/models
 fi
  
 # Execute the command passed into this entrypoint
