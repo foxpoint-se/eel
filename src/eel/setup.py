400e3dce5fd9610599ad70c04c9a40b7c318775e
bulingen <adamlecorney@gmail.com>
2024-01-21 22:26:01 +0100

    feat: navigation node works in docker env

--- src/eel/setup.py
+++ src/eel/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            # "navigation = eel.navigation.navigation_node:main",
+            "navigation = eel.navigation.navigation_node:main",
             # "gnss = eel.gnss.gnss_node:main",
             "imu = eel.imu.imu_node:main",
             # "rudder = eel.rudder.rudder_node:main",
