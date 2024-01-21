71f857e74a49fa5106451b39c36387509a250b7a
bulingen <adamlecorney@gmail.com>
2024-01-21 23:08:10 +0100

    feat: make depth_control and depth_control_rudder nodes work in docker

--- src/eel/setup.py
+++ src/eel/setup.py
@@ -27,9 +27,9 @@ setup(
             "motor = eel.motor.motor_node:main",
             "tank = eel.tank.tank_node:main",
             "pressure = eel.pressure.pressure_node:main",
-            # "depth_control = eel.depth_control.depth_control_node:main",
+            "depth_control = eel.depth_control.depth_control_node:main",
+            "depth_control_rudder = eel.depth_control.depth_control_node_rudder:main",
             # "battery = eel.battery.battery_node:main",
-            # "depth_control_rudder = eel.depth_control.depth_control_node_rudder:main",
         ],
     },
 )
