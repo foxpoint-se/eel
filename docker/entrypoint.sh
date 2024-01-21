2e629bae4839bd80d2cebfe44d16cf82dbe8fc6c
bulingen <adamlecorney@gmail.com>
2024-01-21 19:54:38 +0100

    feat: simpler setup that builds a build that can be run outside of container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,12 +12,5 @@ then
 #   export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix turtlebot3_gazebo)/share/turtlebot3_gazebo/models
 fi
  
-# Source the overlay workspace, if built
-if [ -f /overlay_ws/install/setup.bash ]
-then
-  source /overlay_ws/install/setup.bash
-#   export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix tb3_worlds)/share/tb3_worlds/models
-fi
- 
 # Execute the command passed into this entrypoint
 exec "$@"
