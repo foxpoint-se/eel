0dae60916efd2d68120c80a3a4ee7aaf9ee72a68
bulingen <adamlecorney@gmail.com>
2024-01-28 00:43:57 +0100

    feat: a lot simpler setup now that dockerfile can build for both platforms

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,42 +1,27 @@
 #!/bin/bash
 
 allowed_ros_distros=("foxy" "humble")
-allowed_architectures=("amd64" "arm64")
 
-current_architecture=$(dpkg --print-architecture)
-
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <architecture> <ros-distro>"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <ros-distro>"
     exit 1
 fi
 
-architecture=$1
-if [[ ! " ${allowed_architectures[@]} " =~ " ${architecture} " ]]; then
-    echo "Error: Invalid architecture. Allowed values: ${allowed_architectures[@]}"
-    echo "Your current architeture is $current_architecture so you probably want to use that."
-    exit 1
-fi
-
-ros_distro=$2
+ros_distro=$1
 if [[ ! " ${allowed_ros_distros[@]} " =~ " ${ros_distro} " ]]; then
     echo "Error: Invalid ROS distro. Allowed values: ${allowed_ros_distros[@]}"
     exit 1
 fi
 
-echo "Building image for architecture: $architecture"
 echo "Using ROS distro: $ros_distro"
 
-declare -A arch_prefixes
-arch_prefixes["amd64"]=""
-arch_prefixes["arm64"]="arm64v8/"
-
-arch_prefix=${arch_prefixes["$architecture"]}
-
-tag=$ros_distro-$architecture
-full_command="docker build --build-arg ARCH_PREFIX=$arch_prefix --build-arg ROS_DISTRO=$ros_distro --target base -t foxpoint/eel:$tag -f ./Dockerfile .."
+tag=$ros_distro
+full_command="docker build --build-arg ROS_DISTRO=$ros_distro --target base -t foxpoint/eel:$tag -f ./Dockerfile .."
 echo "Building Docker image"
 echo $full_command
 
 $full_command
 
 # docker build --platform linux/amd64 --build-arg ROS_DISTRO=$ros_distro --target base -t foxpoint/eel:test -f ./Dockerfile ..
+
+# docker build --build-arg ROS_DISTRO=foxy --target base -t foxpoint/eel:test -f ./Dockerfile ..
