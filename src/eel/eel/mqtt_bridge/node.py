39c10a6c7e995c4b730ffc95245ec2dc73548861
bulingen <adamlecorney@gmail.com>
2024-01-28 12:16:01 +0100

    feat: mqtt bridge node that should work but needs testing

--- src/eel/eel/mqtt_bridge/node.py
+++ src/eel/eel/mqtt_bridge/node.py
@@ -1,22 +1,90 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from typing import Optional, TypedDict, cast
+import json
+
+from awscrt import mqtt, io
+from awsiot import mqtt_connection_builder
 from std_msgs.msg import Float32
-from ..utils.topics import MOTOR_CMD
+from eel_interfaces.msg import ImuStatus
+
+from ..utils.topics import MOTOR_CMD, IMU_STATUS
 
 
-class Motor(Node):
+class CertData(TypedDict):
+    endpoint: str
+    port: int
+    certificatePath: str
+    privateKeyPath: str
+    rootCAPath: str
+    clientID: str
+
+
+# usage:
+# ros2 run eel mqtt_bridge --ros-args -p robot_name:=alen -p path_for_config:=/path/to/config.json
+class MqttBridge(Node):
     def __init__(self):
         super().__init__("mqtt_bridge_node")
 
-        self.motor_publisher = self.create_publisher(Float32, MOTOR_CMD, 10)
+        self.mqtt_conn: Optional[mqtt.Connection] = None
 
-        self.get_logger().info("MQTT bridge node started.")
+        self.declare_parameter("path_for_config", "")
+        self.declare_parameter("robot_name", "")
+
+        # self.motor_publisher = self.create_publisher(Float32, MOTOR_CMD, 10)
+        path_for_config = (
+            self.get_parameter("path_for_config").get_parameter_value().string_value
+        )
+
+        self.robot_name: str = (
+            self.get_parameter("robot_name").get_parameter_value().string_value
+        )
+
+        self.get_logger().info("MQTT bridge node starting...")
+
+        with open(path_for_config) as f:
+            cert_data = json.load(f)
+            cert_data = cast(CertData, cert_data)
+
+        self.get_logger().info("Connecting directly to endpoint")
+        self.connect_to_endpoint(cert_data)
+
+        self.init_subs()
+
+    def connect_to_endpoint(self, cert_data: CertData) -> None:
+        self.get_logger().info(
+            f"Connecting directly to endpoint {cert_data['endpoint']}"
+        )
+        self.mqtt_conn = mqtt_connection_builder.mtls_from_path(
+            endpoint=cert_data["endpoint"],
+            port=cert_data["port"],
+            cert_filepath=cert_data["certificatePath"],
+            pri_key_filepath=cert_data["privateKeyPath"],
+            ca_filepath=cert_data["rootCAPath"],
+            client_id=cert_data["clientID"],
+            http_proxy_options=None,
+        )
+        connected_future = self.mqtt_conn.connect()
+        connected_future.result()
+        self.get_logger().info("Connected!")
+
+    def init_subs(self) -> None:
+        self.imu_subscription = self.create_subscription(
+            ImuStatus, IMU_STATUS, self.imu_status_callback, 10
+        )
+
+    def imu_status_callback(self, msg: ImuStatus) -> None:
+        self.mqtt_conn.publish(
+            topic=f"{self.robot_name}/{IMU_STATUS}",
+            payload=msg,
+            qos=mqtt.QoS.AT_LEAST_ONCE,
+        )
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Motor()
+    node = MqttBridge()
     rclpy.spin(node)
     rclpy.shutdown()
 
