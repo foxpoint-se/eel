6d82e2ee93ecabc640a73ae736fc1cbcd64b33e1
bryndav <d.bryngelsson@gmail.com>
2024-03-21 12:44:09 +0100

    Fix: Remove custom msg type, remove typing

--- src/eel/eel/leakage/leakage_source.py
+++ src/eel/eel/leakage/leakage_source.py
@@ -1,8 +1,7 @@
 from abc import abstractmethod
-from typing import Union
 
 
 class LeakageSource:
     @abstractmethod
-    def read_sensor(self) -> Union[bool, None]:
+    def read_sensor(self):
         pass
