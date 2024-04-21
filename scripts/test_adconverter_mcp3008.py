7332674efd664219dca62afa46516337cab1d5dd
bulingen <adamlecorney@gmail.com>
2024-04-21 23:36:58 +0200

    fix(tank): use same range values in test script as everywhere else

--- scripts/test_adconverter_mcp3008.py
+++ scripts/test_adconverter_mcp3008.py
@@ -12,14 +12,15 @@ from gpiozero import MCP3208
 # Rear motor empty limit: 0.28726651202539366
 # Rear motor fill limit: 0.05945549993895738
 
-channel=1
+channel = 1
 mcp = MCP3208(channel=channel, differential=False, max_voltage=3.3)
 
 # front_floor = 0.66
 # front_ceiling = 0.16
 
-rear_floor = 0.288
+rear_floor = 0.34
 rear_ceiling = 0.005
+
 
 def translate_from_range_to_range(value, from_min, from_max, to_min, to_max):
     # Figure out how 'wide' each range is
@@ -32,9 +33,12 @@ def translate_from_range_to_range(value, from_min, from_max, to_min, to_max):
     # Convert the 0-1 range into a value in the right range.
     return to_min + (value_scaled * right_span)
 
+
 print(f"TEST SCRIPT INIT {channel=} {mcp=}")
 
 
 while True:
-    print(f"Raw value{mcp.value} in percentage {translate_from_range_to_range(mcp.value, rear_floor, rear_ceiling, 0.0, 1.0)}")
+    print(
+        f"Raw value{mcp.value} in percentage {translate_from_range_to_range(mcp.value, rear_floor, rear_ceiling, 0.0, 1.0)}"
+    )
     time.sleep(1)
