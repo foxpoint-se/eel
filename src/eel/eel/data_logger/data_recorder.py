e411a889e109297846d7576c919b3b388e7012a3
bulingen <adamlecorney@gmail.com>
2024-08-27 22:13:24 +0200

    refactor: clean up unused

--- src/eel/eel/data_logger/data_recorder.py
+++ src/eel/eel/data_logger/data_recorder.py
@@ -1,27 +1,6 @@
-# när det kommer ny position
-#   kolla med latest_recorded
-#       om den här är längre bort än 0.5 meter
-#           bank()
-#           spara lokation och djup
-#
-# om det kommer meddelande på /history/all/cmd
-#   publicera på /history/all/status
-#
-# när det finns connectivity
-#   publicera regelbundet på /history/updates/status
-
-# regelbundet, var 10e sekund
-#
-
-# TODO:
-# - simplify
-
-import math
-
 from typing import Callable, List, Union
 from .common import (
     Coord3d,
-    LatLon,
     TimedCoord3d,
     Segment,
 )
@@ -30,11 +9,13 @@ from ..utils.nav import (
 )
 
 
-def get_3d_distance(coord1: Coord3d, coord2: Coord3d) -> float:
-    delta_x = coord1["x"] - coord2["x"]
-    delta_y = coord1["y"] - coord2["y"]
-    delta_z = coord1["z"] - coord2["z"]
-    return math.sqrt(delta_x**2 + delta_y**2 + delta_z**2)
+# TODO:
+# - change from xyz coords to latlon
+# - make it possible to switch on and off recording. can be done from the nav node
+# - change chunk size above and below surface. change both seconds and meters
+# - make it possible to get all the segments at once (in case of reload, for example)
+#   - /history/all/cmd -> publish on /history/all/status ... or something
+#   - publish regularly on /history/updates/status ... or something
 
 
 def get_2d_distance_earth(coord1: Coord3d, coord2: Coord3d) -> float:
@@ -56,31 +37,9 @@ class PathRecorder:
         self.last_recorded_3d_position = None
         self.meters_threshold = meters_threshold
         self.seconds_threshold = seconds_threshold
-        self.current_latlon: Union[LatLon, None] = None
-        self.current_depth: Union[float, None] = None
-        self.seconds_threshold = 2.0
         self.segment_in_progress: Union[Segment, None] = None
-        self.last_finalized_segment: Union[Segment, None] = None
         self.finalized_segments: List[Segment] = []
         self.on_new_segment = on_new_segment
-
-    def _has_moved_significantly(self, next_to_record: Coord3d) -> bool:
-        if self.last_recorded_3d_position is None:
-            return True
-        distance = get_3d_distance(next_to_record, self.last_recorded_3d_position)
-        return distance >= self.meters_threshold
-
-    def update_3d_position(self, new_pos: Coord3d) -> None:
-        self.last_recorded_3d_position = new_pos
-
-    def update_position(self, new_pos: LatLon) -> None:
-        self.current_latlon = new_pos
-
-    def update_depth(self, new_depth: float) -> None:
-        self.current_depth = new_depth
-
-    def update_connectivity(self, new_connectivity: bool) -> None:
-        pass
 
     def init_segment(self, pos: TimedCoord3d) -> Segment:
         return Segment(
@@ -90,10 +49,6 @@ class PathRecorder:
             polyline=[pos],
             started_at_seconds=pos["created_at"],
         )
-
-    def simplify_path(self, path: List[TimedCoord3d]) -> List[TimedCoord3d]:
-        # return [c["coord"] for c in path]
-        return [c for c in path]
 
     def get_path_distance(self, path: List[TimedCoord3d]) -> float:
         accumulated_distance = 0.0
@@ -108,15 +63,19 @@ class PathRecorder:
         start_time = segment["polyline"][0]["created_at"]
         end_time = segment["polyline"][-1]["created_at"]
 
-        path = self.simplify_path(path=segment["polyline"])
-        path_distance = self.get_path_distance(path)
+        # TODO: we could simplify the path here.
+        # but that would involve having to ensure that the two endpoints are correct
+        # as well as setting the depth for each new coordinate, based on some sort of
+        # algorithm to determine it from the adjacent coordinates in the original path
+        # or something
+        path_distance = self.get_path_distance(segment["polyline"])
 
         return Segment(
             started_at_seconds=start_time,
             ended_at_seconds=end_time,
             accumulated_distance=path_distance,
             finalized=True,
-            polyline=path,
+            polyline=segment["polyline"],
         )
 
     def add_pos_to_running(self, segment: Segment, pos: TimedCoord3d) -> None:
@@ -125,42 +84,29 @@ class PathRecorder:
         )
         segment["polyline"].append(pos)
         segment["accumulated_distance"] = total_distance
-        # print("TOTAL DISTANCE", total_distance)
 
     def step(self, new_pos: TimedCoord3d) -> None:
         if not self.segment_in_progress:
-            # print("NO SEGMENT. CREATING ONE")
             self.segment_in_progress = self.init_segment(new_pos)
             return
 
         segment_start = self.segment_in_progress["polyline"][0]["created_at"]
 
-        # print("SEGMENT START", segment_start)
         if new_pos["created_at"] - segment_start < self.seconds_threshold:
-            # print("WITHIN TIMEFRAME", self.seconds_threshold, "ADDING")
             self.add_pos_to_running(self.segment_in_progress, new_pos)
-            # print(self.segment_in_progress)
             return
 
+        finalized_segment = self.finalize_segment(self.segment_in_progress)
+        if len(finalized_segment["polyline"]) > 0:
+            last_from_previous = finalized_segment["polyline"][-1]
+            self.segment_in_progress = self.init_segment(last_from_previous)
+            self.add_pos_to_running(self.segment_in_progress, new_pos)
         else:
-            finalized_segment = self.finalize_segment(self.segment_in_progress)
-            # print("NOT WITHIN TIMEFRAME. FINALIZING", finalized_segment)
-            if len(finalized_segment) > 0:
-                last_from_previous = finalized_segment["polyline"][-1]
-                self.segment_in_progress = self.init_segment(last_from_previous)
-                self.add_pos_to_running(self.segment_in_progress, new_pos)
-            else:
-                self.segment_in_progress = self.init_segment(new_pos)
+            self.segment_in_progress = self.init_segment(new_pos)
 
-            # print(
-            #     "ACCUMULATED DISTANCE FINALIZED",
-            #     finalized_segment["accumulated_distance"],
-            # )
-            if finalized_segment["accumulated_distance"] >= self.meters_threshold:
-                self.last_finalized_segment = finalized_segment
-                self.finalized_segments.append(finalized_segment)
-                self.on_new_segment(finalized_segment)
-            return
+        if finalized_segment["accumulated_distance"] >= self.meters_threshold:
+            self.finalized_segments.append(finalized_segment)
+            self.on_new_segment(finalized_segment)
 
     def get_finalized_segments(self) -> List[Segment]:
         return self.finalized_segments
