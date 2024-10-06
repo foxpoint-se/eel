e00857df68a3b50f441fd821019fa2bab55a4681
gunthix <gunnar.fallberg@gmail.com>
2024-10-06 20:16:32 +0000

    wip(imu): add indoor calibrations and set them on init

--- src/eel/eel/imu/imu_sensor.py
+++ src/eel/eel/imu/imu_sensor.py
@@ -16,6 +16,44 @@ def get_corrected_heading(heading: float) -> float:
     return (heading - 180 + HEADING_CORRECTION) % 360
 
 
+CALIBRATION_1 = {
+    "mag": (193, 80, 84),
+    "gyr": (-2, -7, 1),
+    "acc": (-5, -13, -12),
+}
+
+CALIBRATION_2 = {
+    "mag": (-32576, -32653, -32668),
+    "gyr": (-1, -6, 0),
+    "acc": (-14, -44, -34),
+}
+
+# Indoor calibration 1
+# CALIBRATION COMPLETED
+# Insert these preset offset values into project code:
+#   Offsets_Magnetometer:  (193, 80, 84)
+#   Offsets_Gyroscope:     (-2, -7, 1)
+#   Offsets_Accelerometer: (-5, -13, -12)
+
+# Indoor calibration 2
+# CALIBRATION COMPLETED
+# Insert these preset offset values into project code:
+#   Offsets_Magnetometer:  (-32576, -32653, -32668)
+#   Offsets_Gyroscope:     (-1, -6, 0)
+#   Offsets_Accelerometer: (-14, -44, -34)
+
+
+# @offsets_magnetometer.setter
+# def offsets_magnetometer(self, offsets: Tuple[int, int, int]) -> None:
+#     data = bytearray(6)
+#     struct.pack_into("<hhh", data, 0, *offsets)
+#     self._write_register(_OFFSET_MAGNET_REGISTER, bytes(data))
+
+# offsets_gyroscope
+# offsets_accelerometer
+
+my_calibration = CALIBRATION_1
+
 class ImuSensor:
     def __init__(self):
         import adafruit_bno055
@@ -23,6 +61,9 @@ class ImuSensor:
 
         i2c = board.I2C()
         self.sensor = adafruit_bno055.BNO055_I2C(i2c)
+        self.sensor.offsets_magnetometer = my_calibration["mag"]
+        self.sensor.offsets_gyroscope = my_calibration["gyr"]
+        self.sensor.offsets_accelerometer = my_calibration["acc"]
 
     def get_is_calibrated(self):
         return self.sensor.calibrated or False
