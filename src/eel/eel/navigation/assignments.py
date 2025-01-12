435866398d84fe4fd8d83711c1706784e96555a4
bryndav <d.bryngelsson@gmail.com>
2025-01-12 23:44:15 +0100

    Draft: Initail implementation of cross track error drive

--- src/eel/eel/navigation/assignments.py
+++ src/eel/eel/navigation/assignments.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from math import sin, radians
 from typing import Callable, TypedDict
 from time import time
 from .common import (
@@ -8,6 +9,15 @@ from .common import (
     get_next_rudder_turn,
     TOLERANCE_IN_METERS,
 )
+from ..utils.nav import get_closest_turn_direction
+
+
+def cap_value(value, floor, ceiling):
+    if value > max([ceiling, floor]):
+        return max([ceiling, floor])
+    elif value < min([ceiling, floor]):
+        return min([ceiling, floor])
+    return value
 
 
 class AssignmentProgress(TypedDict):
@@ -41,6 +51,7 @@ class WaypointAndDepth(Assignment):
     def __init__(
         self,
         target_pos: LatLon,
+        start_pos: LatLon,
         depth: float,
         on_set_motor: Callable[[float], None],
         on_set_rudder: Callable[[float], None],
@@ -52,6 +63,7 @@ class WaypointAndDepth(Assignment):
         self.on_set_motor = on_set_motor
         self.on_set_rudder = on_set_rudder
         self.on_set_depth = on_set_depth
+        self._initial_bearing_to_target = get_relative_bearing(start_pos, target_pos)
 
     def step(
         self,
@@ -68,7 +80,26 @@ class WaypointAndDepth(Assignment):
 
         bearing_to_target = get_relative_bearing(current_position, self.target_pos)
 
-        next_rudder_turn = get_next_rudder_turn(current_heading, bearing_to_target)
+        # Desired = Bearing of path - min(corrective turn angel, 90)
+        offset_angle = abs(bearing_to_target - self._initial_bearing_to_target) % 360
+        offset_angle = (
+            360 - offset_angle
+            if offset_angle > 180
+            else offset_angle
+        )
+
+        closest_turn = get_closest_turn_direction(self._initial_bearing_to_target, bearing_to_target)
+        offset = closest_turn * offset_angle
+
+        print(f"Calculated offset is: {offset}")
+
+        cross_track_error = sin(radians(offset)) * distance_to_target
+        next_rudder_turn = cap_value(cross_track_error * 0.05, -1.0, 1.0)
+        
+        print(f"Cross track error: {cross_track_error}m")
+        print(f"Next rudder turn: {next_rudder_turn}")
+
+        #next_rudder_turn = get_next_rudder_turn(current_heading, bearing_to_target)
         self.on_set_rudder(next_rudder_turn)
         return {"distance_to_target": distance_to_target}
 
@@ -84,6 +115,7 @@ class SurfaceAssignment(Assignment):
     def __init__(
         self,
         target_pos: LatLon,
+        start_pos: LatLon,
         depth: float,
         on_set_motor: Callable[[float], None],
         on_set_rudder: Callable[[float], None],
@@ -97,6 +129,7 @@ class SurfaceAssignment(Assignment):
         self.on_set_depth = on_set_depth
         self.last_update_at = time()
         self.seconds_at_surface = 0.0
+        self._initial_bearing_to_target = get_relative_bearing(start_pos, target_pos)
 
     def start(self) -> None:
         self.on_set_depth(0.0)
@@ -125,6 +158,17 @@ class SurfaceAssignment(Assignment):
         bearing_to_target = get_relative_bearing(current_position, self.target_pos)
 
         next_rudder_turn = get_next_rudder_turn(current_heading, bearing_to_target)
+        
+        print(f"Bearing to target: {bearing_to_target}")
+        print(f"Distance to target: {distance_to_target}")
+        print(f"Inital bearing to target: {self._initial_bearing_to_target}")
+
+        offset_angle = bearing_to_target - self._initial_bearing_to_target
+
+        print(f"Angle offset: {offset_angle}")
+
+        # drift_in_meters = 
+        
         self.on_set_rudder(next_rudder_turn)
         self.last_update_at = time()
         return {"distance_to_target": distance_to_target}
