8c84a950372c1f0b5a910da07d51e4157e236e57
bulingen <adamlecorney@gmail.com>
2025-04-26 09:25:59 +0200

    docs: update comments about linear velocity, so we remember the reasoning

--- src/eel/eel/utils/sim.py
+++ src/eel/eel/utils/sim.py
@@ -2,13 +2,9 @@
 # We can alter this one, to speed up simulation.
 # NOTE: When increasing the speed, you will probably have to increase the update
 # frequency in e. g. GNSS and IMU, so that it can react faster.
-# LINEAR_VELOCITY = 1.0
-# LINEAR_VELOCITY = 0.84
-# LINEAR_VELOCITY = 0.89
-# LINEAR_VELOCITY = 0.98
-# LINEAR_VELOCITY = 0.92
-
 # NOTE: we measured 0.858 mps in gröndal 2 march 2025
+# NOTE: we measured 0.96 mps outside of rotholmen 18 april 2025. But since it travelled in a squiggly pattern,
+# rather than "as the crow flies", we're rounding it to 1.0
 LINEAR_VELOCITY = 1.0
 
 # UNIT: Degrees per second.
